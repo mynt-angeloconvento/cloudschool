abea21bc575afca2bf78080b0eb31c04a3518a8d
Angelo <godfrey-a.convento@gcash.com>
2025-07-18 16:17:52 +0800

    test: update test for edge cases

--- springboot-crashcourse/session2/lab7/myfirstapi/src/test/java/com/example/demo/myfirstapi/repository/ProductRepositoryTest.java
+++ springboot-crashcourse/session2/lab7/myfirstapi/src/test/java/com/example/demo/myfirstapi/repository/ProductRepositoryTest.java
@@ -30,17 +30,31 @@ class ProductRepositoryTest {
     }
 
     @Test
-    void findById() {
+    void findByIdExisting() {
         Optional<Product> result = productRepository.findById(1L);
         Optional<Product> expected = Optional.of(new Product(1L, "Laptop", 1200.0));
         assertEquals(expected,result);
     }
 
     @Test
-    void save() {
+    void findByIdNull() {
+        Optional<Product> result = productRepository.findById(10L);
+        assertEquals(Optional.empty(),result);
+    }
+
+    @Test
+    void saveNewProduct() {
         productRepository.save(new Product("Milk", 25.25));
         Optional<Product> result = productRepository.findById(4L);
         Optional<Product> expected = Optional.of(new Product(4L, "Milk", 25.25));
+        assertEquals(expected,result);
+    }
+
+    @Test
+    void saveExistingProduct() {
+        productRepository.save(new Product(3L,"Milk", 25.25));
+        Optional<Product> result = productRepository.findById(3L);
+        Optional<Product> expected = Optional.of(new Product(3L, "Milk", 25.25));
         assertEquals(expected,result);
     }
 }
